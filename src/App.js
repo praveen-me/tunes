f0bfa6d932e0ac9dd13fc82a006894990ce68e25
praveen-me <praveenkrsaini0@gmail.com>
2019-08-10 21:25:22 +0530

    Updates packages and code spilitting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './scss/App.scss';
-import Header from './components/Header';
-import UploadFiles from './components/UploadFiles';
-import Player from './components/Player';
+import React, { Component, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./scss/App.scss";
+import Header from "./components/Header";
+import UploadFiles from "./components/UploadFiles";
+const Player = lazy(() => import("./components/Player"));
+// import Player from "./components/Player";
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <>
           <Header />
           <Switch>
-            <Route path="/upload" component={UploadFiles}/>
-            <Route path="/" exact component={Player}/>
+            <Suspense fallback={"Loading..."}>
+              <Route path="/upload" component={UploadFiles} />
+              <Route path="/" exact component={Player} />
+            </Suspense>
           </Switch>
-        </div>
+        </>
       </Router>
     );
   }
