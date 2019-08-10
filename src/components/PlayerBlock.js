dd44ae35777984ae3a734112fb55562ec458f762
praveen-me <praveenkrsaini0@gmail.com>
2019-08-11 00:28:05 +0530

    Resolve issue of audio visualizer and analyzer in chrome

--- src/components/PlayerBlock.js
+++ src/components/PlayerBlock.js
@@ -1,88 +1,104 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { setPreviousToCurrent, setNextToCurrent } from '../store/actions/action';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  setPreviousToCurrent,
+  setNextToCurrent
+} from "../store/actions/action";
 
 class PlayerBlock extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isPlaying : this.props.isPlaying, 
-      musicId : this.props.currentSong.id,
-      currentSongSrc : this.props.currentSong.src
-    }
+      isPlaying: this.props.isPlaying,
+      musicId: this.props.currentSong.id,
+      currentSongSrc: this.props.currentSong.src
+    };
+    this.myMap = new WeakMap();
+
+    this.data = {
+      context: "",
+      src: ""
+    };
   }
-  
+
   componentDidMount() {
     this.setState({
-      isPlaying : true
-    })
+      isPlaying: true
+    });
+    this.initializeVisualizer();
   }
-  
 
   handlePlay = e => {
     e.preventDefault();
-    const song = document.querySelector('.audio');
+    const song = document.querySelector(".audio");
     song.play();
     this.setState({
-      isPlaying : true
-    })
-    this.handleVisualizer()
-  }
+      isPlaying: true
+    });
+  };
 
   handlePause = e => {
     e.preventDefault();
-    const song = document.querySelector('.audio');
+    const song = document.querySelector(".audio");
     this.setState({
-      isPlaying : false
-    })
+      isPlaying: false
+    });
     song.pause();
-    
-  }
-  
+  };
+
   handlePrevious = e => {
     e.preventDefault();
-    this.props.dispatch(setPreviousToCurrent(this.props.currentSong.id))
+    this.props.dispatch(setPreviousToCurrent(this.props.currentSong.id));
 
     this.setState({
-      isPlaying : true
-    })
-
-    this.handleVisualizer()
-  }
+      isPlaying: true
+    });
+  };
 
   handleNext = e => {
     e.preventDefault();
-    console.log(this.state.musicId)
-    this.props.dispatch(setNextToCurrent(this.props.currentSong.id))
+    this.props.dispatch(setNextToCurrent(this.props.currentSong.id));
 
     this.setState({
-      isPlaying : true
-    })
+      isPlaying: true
+    });
+  };
 
-    this.handleVisualizer()
-  }
+  initializeVisualizer() {
+    let context;
+    if (this.data.context) {
+      context = this.data.context;
+    } else {
+      this.data.context = new AudioContext();
+      context = this.data.context;
+    }
 
-  handleVisualizer() {    
-    console.log('visualize');
-    const context = new AudioContext();
-    const audio = document.querySelector('.audio');
-    console.log(audio)
-    if(audio) {
+    const audio = document.querySelector(".audio");
+
+    if (audio) {
       const analyser = context.createAnalyser();
-      let src = context.createMediaElementSource(audio);
+      let src;
+
+      if (this.data.src) {
+        src = this.data.src;
+      } else {
+        this.data.src = context.createMediaElementSource(audio);
+        src = this.data.src;
+      }
+
       const canvas = document.getElementById("canvas");
-      if(canvas) {
+      if (canvas) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         const ctx = canvas.getContext("2d");
 
         src.connect(analyser);
+
         analyser.connect(context.destination);
 
         analyser.fftSize = 512;
 
         let bufferLength = analyser.frequencyBinCount;
-        console.log(bufferLength);
         let dataArray = new Uint8Array(bufferLength);
 
         const WIDTH = canvas.width;
@@ -99,14 +115,14 @@ class PlayerBlock extends Component {
 
           analyser.getByteFrequencyData(dataArray);
 
-          ctx.fillStyle = '#2C3A47';
+          ctx.fillStyle = "#2C3A47";
           ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
           for (let i = 0; i < bufferLength; i++) {
             barHeight = dataArray[i];
-            
-            let r = barHeight + (25 * (i/bufferLength));
-            let g = 250 * (i/bufferLength);
+
+            let r = barHeight + 25 * (i / bufferLength);
+            let g = 250 * (i / bufferLength);
             let b = 50;
 
             ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
@@ -115,62 +131,92 @@ class PlayerBlock extends Component {
             x += barWidth + 1;
           }
         }
-      renderFrame();
+        renderFrame();
       }
     }
   }
 
   render() {
-    const {currentSong} = this.props;
-    let {isPlaying} = this.state;
+    const { currentSong } = this.props;
+    let { isPlaying } = this.state;
 
     return (
       <div className="song-player">
-          {
-            currentSong.src ? (
-              <div className="song-animation">
-                {
-                  isPlaying ?  <img src='https://image.flaticon.com/icons/png/128/26/26805.png' alt="disc" className="rotate"/> :
-                  <img src='https://image.flaticon.com/icons/png/128/26/26805.png' className="rotate rotate-stop" alt="disc"/> 
-                }
-                <canvas id="canvas"></canvas>
-              </div>
-            )
-             : '' 
-          }
-        
+        {currentSong.src ? (
+          <div className="song-animation">
+            {isPlaying ? (
+              <img
+                src="https://image.flaticon.com/icons/png/128/26/26805.png"
+                alt="disc"
+                className="rotate"
+              />
+            ) : (
+              <img
+                src="https://image.flaticon.com/icons/png/128/26/26805.png"
+                className="rotate rotate-stop"
+                alt="disc"
+              />
+            )}
+            <canvas id="canvas" />
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="player-info">
-            {
-              currentSong.src ?  (
-                <div className="player-block">
-                  <audio src={currentSong.src} controls="controls" autoPlay className="audio"/>
-                  <div className="song-title">{currentSong.file.name}</div>
-                  <div className="music-player" id={currentSong.id}>
-                    <div className="player-function">
-                      <a href="#" className="player-link" 
-                      id={currentSong.id} 
-                      onClick={this.handlePrevious}>
-                        <i className="fas fa-backward"></i>
-                      </a>
-                    </div>
-                      {
-                        isPlaying ? 
-                          <div className="player-function">
-                            <a href="#" className="player-link" onClick={this.handlePause}><i className="fas fa-pause"></i></a>
-                          </div>
-                          : 
-                          <div className="player-function">
-                            <a href="#" className="player-link" onClick={this.handlePlay}><i className="fas fa-play"></i></a>
-                          </div>
-                      }
-                      <div className="player-function">
-                        <a href="#" className="player-link" id={currentSong.id} onClick={this.handleNext}><i className="fas fa-forward"></i></a>
-                      </div>
-                    </div>
-                    {this.handleVisualizer()}
+          {currentSong.src ? (
+            <div className="player-block">
+              <audio
+                src={currentSong.src}
+                controls="controls"
+                autoPlay
+                className="audio"
+              />
+              <div className="song-title">{currentSong.file.name}</div>
+              <div className="music-player" id={currentSong.id}>
+                <div className="player-function">
+                  <button
+                    href="#"
+                    className="player-link"
+                    id={currentSong.id}
+                    onClick={this.handlePrevious}>
+                    <i className="fas fa-backward" />
+                  </button>
+                </div>
+                {isPlaying ? (
+                  <div className="player-function">
+                    <button
+                      href="#"
+                      className="player-link"
+                      onClick={this.handlePause}>
+                      <i className="fas fa-pause" />
+                    </button>
                   </div>
-              ) : ''
-            }
+                ) : (
+                  <div className="player-function">
+                    <button
+                      href="#"
+                      className="player-link"
+                      onClick={this.handlePlay}>
+                      <i className="fas fa-play" />
+                    </button>
+                  </div>
+                )}
+                <div className="player-function">
+                  <a
+                    href="#"
+                    className="player-link"
+                    id={currentSong.id}
+                    onClick={this.handleNext}>
+                    <i className="fas fa-forward" />
+                  </a>
+                </div>
+              </div>
+              {this.initializeVisualizer()}
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     );
@@ -179,9 +225,9 @@ class PlayerBlock extends Component {
 
 function mapStateToProps(state) {
   return {
-    currentSong : state.currentSong,
-    isPlaying : state.isPlaying
-  }
+    currentSong: state.currentSong,
+    isPlaying: state.isPlaying
+  };
 }
 
 export default connect(mapStateToProps)(PlayerBlock);
